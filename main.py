6924f1584dbbf7b14d993c79bad0eabccad10d92
Семен <semenionin@yandex.ru>
2023-01-12 12:32:36 +0300

    #

--- main.py
+++ main.py
@@ -39,12 +39,6 @@ def create_parameters_for_task(task_id: int, request: schemas.ParameterCreate, d
     return crud.create_task_parameter(db = db, params = request, task_id = task_id)
 
 
-@app.get("/parameters/", response_model=List[schemas.Parameter])
-def read_parameters(db: Session = Depends(get_db)):
-    parameters = crud.get_parameters(db)
-    return parameters
-
-
 @app.put("/task/{task_sid}")
 def update_task(task_sid, Task: schemas.TaskCreate,  Parameters: schemas.ParameterBase, db: Session = Depends(get_db)):
     return crud.update(task_sid = task_sid, db = db, task = Task, parameters = Parameters)
